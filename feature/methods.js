e84c56cd56188b54637027b96baee4763d6f2743
Quinn Michaels <quinn@indra.ai>
2025-09-08 19:08:56 -0700

    debug the feature method for license_check

--- feature/methods.js
+++ feature/methods.js
@@ -8,31 +8,31 @@ export default {
 		const license = await this.methods.sign('license', 'default', packet);
 		return license;
 	},
-	async license_check(personal, package) {
-		this.state('license', `check:personal:${package.uid}`);
-		if !(personal) return false;
-		this.state('license', `check:package:${package.uid}`);
-		if !(package) return false;
-		this.state('license', `check:uid:${package.uid}`);
-		if (personal.uid !== package.uid) ? return false;
-		this.state('license', `check:time:${package.uid}`);
-		if (personal.time !== package.time) ? return false;
-		this.state('license', `check:date:${package.uid}`);
-		if (personal.date !== package.date) ? return false;
-		this.state('license', `check:md5:${package.uid}`);
-		if (personal.md5 !== package.md5) ? return false;
-		this.state('license', `check:sha256:${package.uid}`);
-		if (personal.sha256 !== package.sha256) ? return false;
-		this.state('license', `check:sha512:${package.uid}`);
-		if (personal.sha512 !== package.sha512) ? return false;
+	async license_check(personalVLA, packageVLA) {
+		this.state('license', `check:personalVLA:${packageVLA.uid}`);
+		if (!personalVLA) return false;
+		this.state('license', `check:packageVLA:${packageVLA.uid}`);
+		if (!packageVLA) return false;
+		this.state('license', `check:uid:${packageVLA.uid}`);
+		if (personalVLA.uid !== packageVLA.uid) return false;
+		this.state('license', `check:time:${packageVLA.uid}`);
+		if (personalVLA.time !== packageVLA.time) return false;
+		this.state('license', `check:date:${packageVLA.uid}`);
+		if (personalVLA.date !== packageVLA.date) return false;
+		this.state('license', `check:md5:${packageVLA.uid}`);
+		if (personalVLA.md5 !== packageVLA.md5) return false;
+		this.state('license', `check:sha256:${packageVLA.uid}`);
+		if (personalVLA.sha256 !== packageVLA.sha256) return false;
+		this.state('license', `check:sha512:${packageVLA.uid}`);
+		if (personalVLA.sha512 !== packageVLA.sha512) return false;
 		
 		// this is to ensure no additional information is being transmitted.
-		this.state('license', `compare:sha256:${package.uid}`);
-		const personal_hash = this.lib.hash('sha256', personal);
-		const package_hash = this.lib.hash('sha256', package);
-		if (personal_hash !== package_hash) return false;
+		this.state('license', `compare:sha256:${packageVLA.uid}`);
+		const personalVLA_hash = this.lib.hash(personalVLA, 'sha256');
+		const packageVLA_hash = this.lib.hash(packageVLA, 'sha256');
+		if (personalVLA_hash !== packageVLA_hash) return false;
 
-		this.state('return', `license:${package.uid}`);
-		return personal;
+		this.state('return', `license:${packageVLA.uid}`);
+		return personalVLA;
 	}
 };
