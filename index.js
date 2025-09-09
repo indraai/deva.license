d0f567733e36cbb8d9f1af7591c944de0a6fa92f
Quinn Michaels <quinn@indra.ai>
2025-09-09 04:48:11 -0700

    update onInit

--- index.js
+++ index.js
@@ -215,13 +215,14 @@ const LICENSE = new Deva({
     },
   },
   async onInit(data, resolve) {
-    const {concerns, global, personal} = this.license(); // get the license config
+    // check license
+    const {personal} = this.license(); // get the license config
     this.vars.license = await this.methods.license_check(personal, pkg.VLA);
     if (this.vars.license) {
-      return this.start(data, resolve);
+      return this.start(data, resolve); // start load if license valid
     }
     else {       
-      return  this.stop(data, resolve);
+      return  this.stop(data, resolve); // stop load if license is invalid.
     }
   },
   async onReady(data, resolve) {
